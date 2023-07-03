0504288bee8377bd882790178f551419b12bf544
nandhininadimuthu123 <nandhukarnaa@gmail.com>
2023-07-03 15:33:30 +0000

    done

--- springapp/src/main/java/com/springapp/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/springapp/springapp/SpringappApplication.java
@@ -10,7 +10,7 @@ public class SpringappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 		System.out.print("Movie Review Aggregator");
-		
+		System.out.print("Movie");
 	}
 
 }
