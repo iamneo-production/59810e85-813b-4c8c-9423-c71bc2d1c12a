b60bac1100fc40590fda16607701efe13193cf07
Somnath4520 <somnathmandal452000@gmail.com>
2023-07-04 08:38:11 +0000

    movie backend code

---
+++ springapp/src/main/java/com/springapp/springapp/Repository/MovieRepository.java
@@ -0,0 +1,15 @@
+package com.springapp.springapp;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.springapp.springapp.Movie;
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	@Query("SELECT u from Movie u")
+	ArrayList<Movie> findAllMovie();
+}
