8af7c130d2f26f4d5d22556f68e7c455fdc7916b
aishwaryaravichandran212002 <aishwaryaravichandran212002@gmail.com>
2023-07-12 11:44:49 +0000

    Changes Made in User ts

--- angularapp/src/app/Model/User.ts
+++ angularapp/src/app/Model/User.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-
 export class User {
     id!:number;
     email!:string;
     password!:string
     constructor(){}
 }
-=======
-export class User{
-  username:string='';
-  email:string='';
-  password:string='';
-}
->>>>>>> 7245517b8d0eea30f7efcebfd528bed4e0e959a3
