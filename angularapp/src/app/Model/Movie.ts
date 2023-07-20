b4b51903583313a782c3e9ae649a86e1e4d79b1b
Gokulnathan0605 <gokulnathan480@gmail.com>
2023-07-20 07:24:18 +0000

    delete all

--- angularapp/src/app/Model/Movie.ts
+++
@@ -1,9 +0,0 @@
-export class Movie{
-    id:number=0;
-    title:string='';
-    releaseDate:Date=new Date();
-    rating:string='';
-    genre:string='';
-    plotSummary:string='';
-    cast:string='';
-}
--- angularapp/src/app/Model/Review.ts
+++
@@ -1,9 +0,0 @@
-export class Review{
-    id:number=0;
-    userId:number=0;
-    rating:string ='';
-    reviewNote:string='';
-    source: string='';
-    date: Date | undefined;
-    movieId:number=0;
-}
