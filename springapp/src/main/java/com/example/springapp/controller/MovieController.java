cd24a5118660db5519bb36bab91a077ee5cb34e4
Gokulnathan0605 <gokulnathan480@gmail.com>
2023-07-20 09:51:11 +0000

    comment

--- springapp/src/main/java/com/example/springapp/controller/MovieController.java
+++ springapp/src/main/java/com/example/springapp/controller/MovieController.java
@@ -43,14 +43,14 @@ public class MovieController {
 	//GET A MOVIE BY MOVIE ID****************************************
 	
 	@GetMapping("/GET/movie/{id}")
-    public ResponseEntity<Movie> getMovieById(@PathVariable("id") Long id) {
-        Movie movie = movieService.getMovieById(id);
-        if (movie != null) {
-            return new ResponseEntity<>(movie, HttpStatus.OK);
-        } else {
+    	public ResponseEntity<Movie> getMovieById(@PathVariable("id") Long id) {
+        	Movie movie = movieService.getMovieById(id);
+       		if (movie != null) {
+            	return new ResponseEntity<>(movie, HttpStatus.OK);
+        	} else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+       		}
+    	}
 
 	// @GetMapping("/GET/movie/{id}")
 	// public ResponseEntity<?> showMovieById(@PathVariable long id){
