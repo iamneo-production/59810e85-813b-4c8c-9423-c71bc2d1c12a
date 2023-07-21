13dd944aaed7071aebc56be9e0caeb7d9dad15c6
Gokulnathan0605 <gokulnathan480@gmail.com>
2023-07-21 09:49:16 +0000

    review list commit

---
+++ angularapp/src/app/review-list/review-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Review } from '../Model/Review';
+import { ReviewListserviceService } from '../Services/review-listservice.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-review-list',
+  templateUrl: './review-list.component.html',
+  styleUrls: ['./review-list.component.css']
+})
+export class ReviewListComponent implements OnInit {
+  movieId: number = 3; //Example movie ID
+  review: Review = new Review() ;
+  reviews: Review[]=[];
+
+  constructor(private reviewService: ReviewListserviceService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.getReviews();
+  }
+
+  getReviews(): void {
+    this.reviewService.getReviews( ).subscribe(
+      reviews => this.reviews = reviews);
+      console.log(this.reviews);
+  }
+
+
+
+//Button For Review
+  gotoReviewForm() {
+    this.router.navigateByUrl('ReviewForm');
+  }
+
+}
