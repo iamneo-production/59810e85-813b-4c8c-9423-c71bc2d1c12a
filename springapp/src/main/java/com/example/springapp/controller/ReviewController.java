79ce42940cea9ae02170372d3e509c5dfef9c7f8
Somnath4520 <somnathmandal452000@gmail.com>
2023-07-21 15:51:48 +0000

    home component added

--- springapp/src/main/java/com/example/springapp/controller/ReviewController.java
+++ springapp/src/main/java/com/example/springapp/controller/ReviewController.java
@@ -18,7 +18,7 @@ import com.example.springapp.model.Review;
 import com.example.springapp.service.ReviewService;
 
 @RestController
-// @CrossOrigin(origins="https://8081-fecfecbdcfcdafbecacdccdbbeeaeaadbdbabf.project.examly.io/")   //Somnath 
+// @CrossOrigin(origins="https://8080-fecfecbdcfcdafbecacdccdbbedbeeabbebbdd.project.examly.io/")   //Somnath 
 @CrossOrigin(origins = "https://8081-cdcafaaaeecacdccdbbeeaeaadbdbabf.project.examly.io/")         //Aishwarya Ghosh
 // @CrossOrigin(origins = "")     //Sakthivel
 // @CrossOrigin(origins = "")     //Ragavi
