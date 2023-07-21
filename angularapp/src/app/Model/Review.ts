141b5007a9d238f3dc7b574855f3b6e4b2404f83
Gokulnathan0605 <gokulnathan480@gmail.com>
2023-07-21 09:41:35 +0000

    model commit

---
+++ angularapp/src/app/Model/Review.ts
@@ -0,0 +1,9 @@
+export class Review{
+    id:number=0;
+    userId:number=0;
+    rating:string ='';
+    reviewNote:string='';
+    source: string='';
+    date: Date | undefined;
+    movieId:number=0;
+}
