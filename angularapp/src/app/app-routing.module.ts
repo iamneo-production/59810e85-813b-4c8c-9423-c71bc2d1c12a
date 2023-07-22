9b004274ae811a4731557f9b2b16af112d5c2ebf
Gokulnathan0605 <gokulnathan480@gmail.com>
2023-07-22 14:56:30 +0000

    git

---
+++ angularapp/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { ReviewFormComponent } from './review-form/review-form.component';
+import { ReviewUpdateFormComponent } from './review-update-form/review-update-form.component';
+import { ReviewListComponent } from './review-list/review-list.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+const routes: Routes = [
+  {path:"",component:HomeComponentComponent},
+  {
+    path:"searchmovie",
+    component:MovieListComponent,
+    pathMatch:"full"
+  },
+  {path:"movies", component:MovieListComponent,pathMatch:"full"},
+  {path:"ReviewList/:id",component:ReviewListComponent},
+  {path:'ReviewForm', component:ReviewFormComponent},
+  {path:'ReviewUpdate/:id',component:ReviewUpdateFormComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
