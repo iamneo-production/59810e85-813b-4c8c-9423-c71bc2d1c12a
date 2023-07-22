0edaa48d2856c6389b2db3b46b7c66c35c97cae5
Gokulnathan0605 <97106980+Gokulnathan0605@users.noreply.github.com>
2023-07-22 20:49:06 +0530

    Add files via upload

--- angularapp/src/app/newmovie/newmovie.component.ts
+++ angularapp/src/app/newmovie/newmovie.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NewmovieService } from '../Services/newmovie.service';
 
 @Component({
   selector: 'app-newmovie',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./newmovie.component.css']
 })
 export class NewmovieComponent {
+  fg = new FormGroup({
+    id: new FormControl(""),
+    title: new FormControl(""),
+    genre: new FormControl(""),
+    rating: new FormControl(""),
+    starcast: new FormControl(""),
+    releasedate: new FormControl(""),
+    plotsummary: new FormControl("")
+   
+
+  })
+
+  constructor(private handler : NewmovieService) {
+
+  }
+  Generlist=['Anime','Crime','Drama','Horror','Kids & Family','Science Fiction','Sports','Thiriller','War']
+  item : any = "";
+  addMovie() {
+    let id = this.fg.controls.id.value;
+    let title = this.fg.controls.title.value;
+    let genre = this.fg.controls.genre.value;
+    let rating = this.fg.controls.rating.value;
+    let starcast = this.fg.controls.starcast.value;
+    let releasedate = this.fg.controls.releasedate.value;
+    let plotsummary = this.fg.controls.plotsummary.value;
+  
+    
+    console.log(this.item);
+
+    let moviedetail = {
+      id:id,
+      title: title,
+      genre: genre,
+      rating: rating,
+      starcast: starcast,
+      releasedate:  releasedate,
+      plotsummary: plotsummary,
+    
+    };
+   
+    this.handler.addMovieByService(moviedetail)
+    console.log(id,title,genre,rating,starcast,releasedate,plotsummary);
+  }
+
 
 }
