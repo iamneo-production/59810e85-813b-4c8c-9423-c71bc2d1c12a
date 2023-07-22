4606cea7bb35ac3545fba47c41ca8ccec2557b63
Gokulnathan0605 <97106980+Gokulnathan0605@users.noreply.github.com>
2023-07-22 20:29:22 +0530

    Add files via upload

---
+++ angularapp/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReviewListserviceService } from './Services/review-listservice.service';
+import { ReviewServiceService } from './Services/review-service.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'angularapp';
+
+  searchResults: string[] = [];
+
+  constructor(private router: Router, private service: ReviewListserviceService, private revService: ReviewServiceService) { }
+
+  performSearch(searchTerm: string): void {
+    this.searchResults = [];
+  }
+
+
+
+
+  goToReviewList(movId: number) {
+    this.router.navigate(['ReviewList', movId]);
+    this.service.getMovieId(movId);
+  }
+
+
+  //Review Update form
+  gotoReviewUpdate(revId: number) {
+    this.router.navigate(['ReviewUpdate', revId]);
+    this.revService.getId(revId);
+  }
+
+}
