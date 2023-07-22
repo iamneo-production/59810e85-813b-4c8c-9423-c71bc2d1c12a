b0df8c32af38f91bea1f15d153ada5ce84cb9852
Gokulnathan0605 <97106980+Gokulnathan0605@users.noreply.github.com>
2023-07-22 20:52:26 +0530

    Add files via upload

--- angularapp/src/app/user-nav/user-nav.component.ts
+++ angularapp/src/app/user-nav/user-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Movie } from '../Model/Movie';
+import { MovieService } from '../Services/movie-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-nav',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class UserNavComponent {
 
+  allMovies:Movie[]=[];
+  
+  constructor(private movieService:MovieService, private router:Router){}
+
+  fetchAllMovies(){
+    this.router.navigateByUrl('movies');
+  }
 }
