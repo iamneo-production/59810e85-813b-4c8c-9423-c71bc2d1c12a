949267f7ec21ebd45d0eaa4f393ec630d0061db3
Gokulnathan0605 <gokulnathan480@gmail.com>
2023-07-22 13:53:30 +0000

    commit change

--- angularapp/src/app/Services/movie-service.service.ts
+++ angularapp/src/app/Services/movie-service.service.ts
@@ -17,6 +17,8 @@ export class MovieService{
     // private getUrl = "https://8080-fecfecbdcfcdafbecacdccdbbedbeeabbebbdd.project.examly.io/movie";
     // private postUrl = "https://8080-fecfecbdcfcdafbecacdccdbbedbeeabbebbdd.project.examly.io/POST/movie";
     // private searchUrl = "https://8080-fecfecbdcfcdafbecacdccdbbedbeeabbebbdd.project.examly.io/GET/movie/search";
+    
+    // GOKULNATHAN
     private getUrl = "https://8080-aaffdaaaacdbfdecacdccdbbeeaeaadbdbabf.project.examly.io/movie";
     private postUrl = "https://8080-aaffdaaaacdbfdecacdccdbbeeaeaadbdbabf.project.examly.io/POST/movie";
     private searchUrl = "https://8080-aaffdaaaacdbfdecacdccdbbeeaeaadbdbabf.project.examly.io/GET/movie/search";
--- angularapp/src/app/movie-details/movie-details.component.ts
+++ angularapp/src/app/movie-details/movie-details.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MovieService } from '../Services/movie-service.service';
 import { ReviewServiceService } from '../Services/review-service.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Movie } from '../Model/Movie';
 
 @Component({
@@ -10,43 +10,22 @@ import { Movie } from '../Model/Movie';
   styleUrls: ['./movie-details.component.css']
 })
 
-export class MovieDetailsComponent {
+export class MovieDetailsComponent{
 
-  searchTerm:string="";
   movie: Movie =new Movie();
   allMovies:Movie[]=[];
 
   constructor(private movieService:MovieService ,private reviewform: ReviewServiceService, private router:Router,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // this.getMovieById()
-    
+    // this.getMovieDetails(); 
+    // this.getMovieById();
+    // this.getMovies();
   }
-  
-  // getMovieDetails() {
-  //   const movieId = this.route.snapshot.paramMap.get('id'); 
-  //   if (movieId) {
-  //     const id = parseInt(movieId, 10);
-  //     this.movieService.getMovieById(id).subscribe((data: Movie) => {
-  //       this.movie = data;
-  //     });
-  //   }
-  // }
 
-  getMovieById(){
-    this.movieService.getMovieById().subscribe(data =>{
-      this.movie.id = data.id;
-      this.movie.title = data.title;
-      this.movie.releaseDate =data.releaseDate;
-      this.movie.rating = data.rating;
-      this.movie.genre = data.genre;
-      this.movie.plotSummary = data.plotSummary;
-      this.movie.cast = data.cast;
-    })
-  }
-  
-   //Button For Review
-   gotoReviewForm(movieid:number,userid:number) {
+
+  //Button For Review
+  gotoReviewForm(movieid:number,userid:number) {
     this.router.navigateByUrl('ReviewForm');
     this.reviewform.getUserMovieId(movieid,userid)
   }
