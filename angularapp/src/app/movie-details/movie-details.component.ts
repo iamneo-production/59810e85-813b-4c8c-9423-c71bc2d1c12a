5b567d13a3707ce84d4739395d2a012a59741922
Somnath4520 <somnathmandal452000@gmail.com>
2023-07-23 06:24:52 +0000

    movie details

---
+++ angularapp/src/app/movie-details/movie-details.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Movie } from '../Model/Movie';
+import { MovieService } from '../Services/movie-service.service';
+import { Router } from '@angular/router';
+import { ReviewServiceService } from '../Services/review-service.service';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.css']
+})
+export class MovieDetailsComponent {
+
+  movie: Movie =new Movie();
+  allMovies:Movie[]=[];
+  
+
+  constructor(private movieService : MovieService, private reviewform: ReviewServiceService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.getMovie();
+  }
+
+  private getMovie(){
+    this.movieService.getMovieById(Number(localStorage.getItem('movieId'))).subscribe(data=>{
+      this.movie = data;
+      console.log(this.movie);
+    })
+  }
+
+  //Button For Review
+  gotoReviewForm(movieid:number,userid:number) {
+    this.router.navigateByUrl('ReviewForm');
+    this.reviewform.getUserMovieId(movieid,userid)
+  }
+}
