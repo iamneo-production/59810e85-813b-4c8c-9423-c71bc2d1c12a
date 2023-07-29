d4891d888141b04490ea4339aaa1e3a99f7fd37c
Sakthivel78 <sakthivelbabu2001@gmail.com>
2023-07-29 10:44:12 +0000

    changes in admin

--- angularapp/src/app/newmovie/newmovie.component.ts
+++ angularapp/src/app/newmovie/newmovie.component.ts
@@ -48,7 +48,6 @@ export class NewmovieComponent {
     };
    
     this.handler.addMovieByService(moviedetail)
-    alert("Successfully added");
     console.log(id,title,genre,rating,starcast,releasedate,plotsummary);
     alert("Movie Added Successfully");
   }
